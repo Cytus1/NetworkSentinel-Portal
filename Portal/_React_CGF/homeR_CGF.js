a7b18811b12165e5246b8137ec2459a9272cceb9
Cytus1 <xyloaex@gmail.com>
2026-05-05 22:48:31 -0400

    set up react routing and home page works

---
+++ Portal/_React_CGF/homeR_CGF.js
@@ -0,0 +1,14 @@
+function homeR_CGF() {
+    return (
+        <div className = "home">
+            <h4>
+                Network Sentinel Portal
+            </h4>
+
+            <p>
+                Welcome!
+            </p>
+        </div>
+
+    )
+}
