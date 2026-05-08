7d8404773c3fbf16a4fea9bdd9b95ca3a3e149ac
Cytus1 <xyloaex@gmail.com>
2026-05-08 00:41:27 -0400

    rotating card portal prototype

--- Portal/_React_CGF/homeR_CGF.js
+++ Portal/_React_CGF/homeR_CGF.js
@@ -2,9 +2,6 @@ function homeR_CGF() {
     return (
         <div className = "home">
             <header className = "navbar">
-                <div className = "portal-title">
-                    Network Sentinel Portal
-                </div>
 
                 <div className = "nav-links">
                     <a className = "nav-link" href = "#">Home</a>
@@ -18,6 +15,139 @@ function homeR_CGF() {
                 <h4>
                     WOW
                 </h4>
+
+                <div className = "portal-title">
+                    Network Sentinel Portal
+                </div>
+
+                <div className = "void" id = "void">
+                    <div className = "crop">
+                        <ul className = "card-list" style = {{"--count": 6}}>
+
+                            <li>
+                                <div className = "card">
+                                    <a href = "#">
+                                        <span> 1046 </span> 
+                                        <span> wow a num </span>
+                                    </a>
+                                </div>
+                            </li>
+
+                            <li>
+                                <div className = "card">
+                                    <a href = "#">
+                                        <img className = "card-img" src = "pics/home-black-ice.gif" alt = "" />
+                                    </a>
+                                </div>
+                            </li>
+
+                            {/* --- */}
+
+                            <li>
+                                <div className = "card">
+                                    <a href = "#">
+                                        <span> 1046 </span> 
+                                        <span> wow a num </span>
+                                    </a>
+                                </div>
+                            </li>
+
+                            <li>
+                                <div className = "card">
+                                    <a href = "#">
+                                        <img className = "card-img" src = "pics/home-black-ice.gif" alt = "" />
+                                    </a>
+                                </div>
+                            </li>
+
+                            {/* --- */}
+
+                            <li>
+                                <div className = "card">
+                                    <a href = "#">
+                                        <span> 1046 </span> 
+                                        <span> wow a num </span>
+                                    </a>
+                                </div>
+                            </li>
+
+                            <li>
+                                <div className = "card">
+                                    <a href = "#">
+                                        <img className = "card-img" src = "pics/home-black-ice.gif" alt = "" />
+                                    </a>
+                                </div>
+                            </li>
+
+                            {/* --- */}
+
+                            <li>
+                                <div className = "card">
+                                    <a href = "#">
+                                        <span> 1046 </span> 
+                                        <span> wow a num </span>
+                                    </a>
+                                </div>
+                            </li>
+
+                            <li>
+                                <div className = "card">
+                                    <a href = "#">
+                                        <img className = "card-img" src = "pics/home-black-ice.gif" alt = "" />
+                                    </a>
+                                </div>
+                            </li>
+
+                            {/* --- */}
+
+                            <li>
+                                <div className = "card">
+                                    <a href = "#">
+                                        <span> 1046 </span> 
+                                        <span> wow a num </span>
+                                    </a>
+                                </div>
+                            </li>
+
+                            <li>
+                                <div className = "card">
+                                    <a href = "#">
+                                        <img className = "card-img" src = "pics/home-black-ice.gif" alt = "" />
+                                    </a>
+                                </div>
+                            </li>
+
+                            {/* --- */}
+
+                            <li>
+                                <div className = "card">
+                                    <a href = "#">
+                                        <span> 1046 </span> 
+                                        <span> wow a num </span>
+                                    </a>
+                                </div>
+                            </li>
+
+                            <li>
+                                <div className = "card">
+                                    <a href = "#">
+                                        <img className = "card-img" src = "pics/home-black-ice.gif" alt = "" />
+                                    </a>
+                                </div>
+                            </li>
+
+                        </ul>
+
+                        <div className = "last-circle"></div>
+                        <div className = "second-circle"></div>
+
+                    </div>
+
+                    <div className = "mask"></div>
+                    <div className = "center-circle"></div>
+
+
+                </div>
 
                 <p>
                     Welcome!
